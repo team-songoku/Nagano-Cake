122333fb5e277123639a083a1781f675b116d790
WakuHonda <105696893+hndwkwksn@users.noreply.github.com>
2022-07-16 10:14:26 +0000

    [Add]配送先テーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_16_095833) do
+ActiveRecord::Schema.define(version: 2022_07_16_101122) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -77,6 +77,15 @@ ActiveRecord::Schema.define(version: 2022_07_16_095833) do
     t.string "shipping_postal_code", null: false
     t.string "shipping_address", null: false
     t.string "shipping_name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "shipping_addresses", force: :cascade do |t|
+    t.integer "customer_id", null: false
+    t.string "name", null: false
+    t.string "address", null: false
+    t.string "postal_code", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
