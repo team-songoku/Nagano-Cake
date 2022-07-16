944b2c270e20e152124e32d471899ac41516815b
wadayuki <106656539+lu3-yk@users.noreply.github.com>
2022-07-16 17:08:00 +0000

    ログイン機能作成中

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
 
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # 顧客用
   # URL /customers/sign_in ...
   devise_for :customers,skip: [:passwords], controllers: {
@@ -13,6 +12,9 @@ Rails.application.routes.draw do
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
 }
+
+root :to =>"homes#top"
+get "home/about"=>"homes#about"
 
   
 end
