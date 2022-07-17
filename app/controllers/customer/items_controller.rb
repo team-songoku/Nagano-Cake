ebbfd3a26605edd2233a9d449839861cbf875f3c
WakuHonda <105696893+hndwkwksn@users.noreply.github.com>
2022-07-17 05:26:25 +0000

    [Add]ルーティング設定

--- app/controllers/customer/items_controller.rb
+++ app/controllers/customer/items_controller.rb
@@ -1,5 +1,14 @@
 class Customer::ItemsController < ApplicationController
   def top
-      @items = Item.all
+  end
+
+  def index
+  end
+
+
+  def show
+  end
+
+  def about
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,18 @@ Rails.application.routes.draw do
   sessions: 'customer/sessions'
 }
 
+  # scope module: 'customers' do
+  #   root 'items#top'
+  #   resources :items, only: [:show, :index]
+  #   get 'about' => 'items#about'
+  # end
+
+
+ namespace :customer do
+   root 'items#top'
+   resources :items
+ end
+
   # 管理者用
   # URL /admin/sign_in ...
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
@@ -14,9 +26,7 @@ Rails.application.routes.draw do
 }
 
 
-root :to =>"homes#top"
-get "home/about"=>"homes#about"
+# root :to =>"homes#top"
+# get "home/about"=>"homes#about"
 
-
-  
 end
