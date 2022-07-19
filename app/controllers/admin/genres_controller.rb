4794dab5689acb250f5625e4854b6149b5fc5958
EC2 Default User <kazukipump7@gmail.com>
2022-07-19 14:55:26 +0000

    ジャンル新規登録フォーム

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,7 +1,30 @@
 class Admin::GenresController < ApplicationController
+
   def index
+    @genre = Genre.new
+  end
+
+  def create
+    @genre = Genre.new(genre_params)
+    if @genre.save
+      redirect_to request.referer
+    else
+      
+      render 'index'
+    end
   end
 
   def edit
+    
+  end
+
+  def update
+  end
+
+
+  private
+
+  def genre_params
+    params.require(:genre).permit(:name)
   end
 end
