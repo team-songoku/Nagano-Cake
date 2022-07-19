5cc0337b087da9795b54d9efefed77736a22e10e
WakuHonda <105696893+hndwkwksn@users.noreply.github.com>
2022-07-19 13:49:17 +0000

    [Add]confirmページ作成

--- app/controllers/customer/orders_controller.rb
+++ app/controllers/customer/orders_controller.rb
@@ -1,5 +1,10 @@
 class Customer::OrdersController < ApplicationController
+  # 注文情報入力画面（支払い方法・届け先）
   def new
+    @order = Order.new
+  end
+
+  def create
   end
 
   def index
@@ -9,8 +14,17 @@ class Customer::OrdersController < ApplicationController
   end
 
   def confirm
+    @cart_items = CartItem.all
+    @total =  @cart_items.inject(0) { |sum, item| sum + item.subtotal }
+    @order = current_customer.order.new(order_params)
   end
 
   def complete
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:payment_method)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -19,7 +19,9 @@ Rails.application.routes.draw do
        delete 'destroy_all'
      end
    end
-   resources :orders
+   resources :orders, only: [:new, :create, :index, :show]
+   post '/orders/confirm' => 'orders#confirm'
+   get '/orders/complete' => 'orders#complete'
    resources :customers, only: [:edit,:update,:destroy]
    resources :shipping_addresses
  end
