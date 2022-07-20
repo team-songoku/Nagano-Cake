0c2ff5267b9d1ecd5eccc99db1b048e95e753c45
EC2 Default User <kazukipump7@gmail.com>
2022-07-20 10:28:07 +0000

    会員情報編集[仮完成]

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -12,15 +12,21 @@ class Admin::CustomersController < ApplicationController
   end
 
   def update
-    
+    if @customer.update(customer_params)
+      redirect_to admin_customer_path(@customer)
+    else
+      render "edit"
+    end
   end
+
 
 
   private
 
-  # def customer_params
-  #   params.require(:customer).permit()
-  # end
+  def customer_params
+      params.require(:customer).permit(:email, :encrypted_password, :last_name, :first_name, :last_name_kana, :first_name_kana, :post_code, :address, :phone_number, :is_deleted)
+  end
+
 
   def before_customer
      @customer = Customer.find(params[:id])
