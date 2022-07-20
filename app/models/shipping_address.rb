a7bf67a347a6a2b8ef42e8ebd9ef977ba7311b14
WakuHonda <105696893+hndwkwksn@users.noreply.github.com>
2022-07-20 03:49:25 +0000

    [Add]order/new.html/登録済み住所

--- app/models/shipping_address.rb
+++ app/models/shipping_address.rb
@@ -1,3 +1,8 @@
 class ShippingAddress < ApplicationRecord
   belongs_to :customer
+
+  #注文情報入力画面の登録済み住所から選択で使用（customer/orders/new.html#line27）
+  def address_display
+  '〒' + postal_code + ' ' + address + ' ' + name
+  end
 end
