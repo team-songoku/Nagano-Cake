b10665781eaadf3af21072e21a60ba051e14f27b
WakuHonda <105696893+hndwkwksn@users.noreply.github.com>
2022-07-21 08:55:02 +0000

    [Start]order履歴機能

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -6,7 +6,7 @@ class Admin::ItemsController < ApplicationController
 
   def create
     @item = Item.new(item_params)
-    if @item.save!
+    if @item.save
       redirect_to admin_item_path(@item)
     else
       @items = Item.all
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ApplicationRecord
   has_one_attached :image
 
-  belongs_to :genre
+#   belongs_to :genre
   has_many :cart_items
   has_many :order_details
 
