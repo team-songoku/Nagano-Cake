363e28bcc525d006797fb6a3e08dfffff26a0de3
WakuHonda <105696893+hndwkwksn@users.noreply.github.com>
2022-07-21 07:52:57 +0000

    [Finish]order機能

--- app/controllers/customer/orders_controller.rb
+++ app/controllers/customer/orders_controller.rb
@@ -25,6 +25,15 @@ class Customer::OrdersController < ApplicationController
     @order = Order.new(order_params)
     @order.customer_id = current_customer.id
     if @order.save
+      current_customer.cart_items.each do |cart_item|
+        @order_detail = OrderDetail.new
+        @order_detail.item_id = cart_item.item_id
+        @order_detail.quantity = cart_item.quantity
+        @order_detail.price = cart_item.item.with_tax_price
+        @order_detail.order_id = @order.id
+        @order_detail.save
+      end
+      current_customer.cart_items.destroy_all
       redirect_to orders_complete_path
     else
       render :new
