c9b007b53fea6dfab6198458592914c84ef5b8f7
WakuHonda <105696893+hndwkwksn@users.noreply.github.com>
2022-07-22 06:16:07 +0000

    [Add]制作ステータス機能追加/自動更新はまだ

--- config/routes.rb
+++ config/routes.rb
@@ -38,6 +38,7 @@ Rails.application.routes.draw do
 namespace :admin do
    resources :items, only: [:new,:index,:create,:show,:edit,:update,:destroy]
    resources :orders, only: [:index,:show,:update]
+   resources :order_details, only: [:update]
    resources :genres, only: [:index,:create,:edit,:update,:destroy]
    resources :customers, only: [:index, :show, :edit, :update]
 end
