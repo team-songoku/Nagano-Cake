2b34a135a569b6f02f5aa001c70f56f3370e4bb0
WakuHonda <105696893+hndwkwksn@users.noreply.github.com>
2022-07-22 02:59:06 +0000

    [Add]admin/order/注文ステータス各ページに反映

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -3,7 +3,23 @@ class Admin::OrdersController < ApplicationController
     @order = Order.find(params[:id])
   end
 
+  def update
+    @order = Order.find(params[:id])
+    if @order.update(order_params)
+      redirect_to admin_order_path(@order.id)
+    else
+      @orders = Order.all
+      render 'index'
+    end
+  end
+
   def index
     @orders = Order.all
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:status)
   end
 end
