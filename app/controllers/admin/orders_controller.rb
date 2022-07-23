d3c448c7b3a7334d9f4fffd9edf17023b0cf09ee
WakuHonda <105696893+hndwkwksn@users.noreply.github.com>
2022-07-23 04:16:08 +0000

    [Add]入金確認の自動更新

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -8,6 +8,12 @@ class Admin::OrdersController < ApplicationController
   def update
     @order = Order.find(params[:id])
     if @order.update(order_params)
+      if @order.status == "payment_confirmation"
+        @order.order_details.each do |order_detail|
+          order_detail.making_status = "waiting"
+          order_detail.save
+        end
+      end
       redirect_to admin_order_path(@order.id)
     else
       @orders = Order.all
