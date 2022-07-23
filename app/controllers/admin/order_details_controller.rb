1db03cc5bdf790d0fa87633b382511676152cb62
WakuHonda <105696893+hndwkwksn@users.noreply.github.com>
2022-07-23 04:36:57 +0000

    [Add]製作中の自動更新

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -4,6 +4,10 @@ class Admin::OrderDetailsController < ApplicationController
   def update
     @order_detail = OrderDetail.find(params[:id])
     if @order_detail.update(order_detail_params)
+      if @order_detail.making_status == "cooking"
+        @order_detail.order.status = "in_making"
+        @order_detail.order.save
+      end
       redirect_to admin_order_path(@order_detail.order.id)
     else
       render 'index'
